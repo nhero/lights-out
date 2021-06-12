bef7a0295fa7ddd839b9025cff5619f02b96e641
Nate Hero <natehero@gmail.com>
2021-06-12 16:09:07 -0600

    cleanup

--- src/components/LightsOut/LightsOut.tsx
+++ src/components/LightsOut/LightsOut.tsx
@@ -1,32 +1,31 @@
-import React, { useState } from 'react';
-import reportWebVitals from '../../reportWebVitals';
-import './LightsOut.scss';
+import React, { useState } from "react";
+import "./LightsOut.scss";
 
 const LightsOut: React.FC = () => {
-
   const getInitialGrid = () => {
     let grid = [];
-    grid[0] = [false,false,true,true,true];
-    grid[1] = [false,true,true,true,true];
-    grid[2] = [true,true,true,true,true];
-    grid[3] = [true,true,true,true,true];
-    grid[4] = [true,true,true,true,true];
+    grid[0] = [false, false, true, true, true];
+    grid[1] = [true, true, false, false, true];
+    grid[2] = [false, true, true, true, true];
+    grid[3] = [true, true, true, true, false];
+    grid[4] = [true, true, false, false, true];
 
     return grid;
   };
 
   const [count, setCount] = useState(0);
+  const [solved, setSolved] = useState(false);
   const [rows, setRows] = useState(getInitialGrid);
 
   const incrementClick = () => {
-    setCount(count + 1)
-  }
+    setCount(count + 1);
+  };
 
   const swapStatus = (rowId: number, colId: number) => {
     let updatedRows = rows;
     updatedRows[rowId][colId] = !updatedRows[rowId][colId];
     setRows(updatedRows);
-  }
+  };
 
   const updateMatrix = (status: Boolean, rowId: number, colId: number) => {
     incrementClick();
@@ -37,11 +36,11 @@ const LightsOut: React.FC = () => {
     }
 
     // update current row
-    if (colId - 1 >= 0 ) {
+    if (colId - 1 >= 0) {
       swapStatus(rowId, colId - 1);
     }
     swapStatus(rowId, colId);
-    if (colId + 1 >= 0 ) {
+    if (colId + 1 >= 0) {
       swapStatus(rowId, colId + 1);
     }
 
@@ -51,7 +50,7 @@ const LightsOut: React.FC = () => {
     }
 
     checkIfSolved();
-  }
+  };
 
   const getCols = (rowId: number, numCols: number) => {
     let columnData: any = [];
@@ -59,18 +58,42 @@ const LightsOut: React.FC = () => {
     for (let col = 0; col < numCols; col++) {
       toggleClass = rows[rowId][col] === false ? "off" : "on";
       if (col % 2 === 0) {
-        columnData.push(<div onClick={() => updateMatrix(rows[rowId][col], rowId, col)} className={`col shadow ${toggleClass}`}></div>);
+        columnData.push(
+          <div
+            onClick={() => updateMatrix(rows[rowId][col], rowId, col)}
+            className={`col shadow ${toggleClass}`}
+          ></div>
+        );
       } else {
-        columnData.push(<div onClick={() => updateMatrix(rows[rowId][col], rowId, col)} className={`col shadow ${toggleClass}`}></div>);
+        columnData.push(
+          <div
+            onClick={() => updateMatrix(rows[rowId][col], rowId, col)}
+            className={`col shadow ${toggleClass}`}
+          ></div>
+        );
       }
     }
 
     return columnData;
-  }
+  };
 
   const checkIfSolved = () => {
+    if (
+      !rows[0].includes(true) &&
+      !rows[1].includes(true) &&
+      !rows[2].includes(true) &&
+      !rows[3].includes(true) &&
+      !rows[4].includes(true)
+    ) {
+      setSolved(true);
+    }
+  };
 
-  }
+  const reset = () => {
+    setSolved(false);
+    setCount(0);
+    setRows(getInitialGrid());
+  };
 
   const getRows = () => {
     const gridLength = rows.length;
@@ -79,22 +102,23 @@ const LightsOut: React.FC = () => {
     let rowData = [];
 
     for (let row = 0; row < 5; row++) {
-      rowData.push(
-        <div className="row">{getCols(row, numCols)}</div>
-      );
+      rowData.push(<div className="row">{getCols(row, numCols)}</div>);
     }
 
     return <React.Fragment>{rowData}</React.Fragment>;
-  }
+  };
 
   return (
     <div>
-      <h2>{count} clicks</h2>
+      <h2>
+        {solved ? `You solved the puzzle in ${count} trys!` : `${count} clicks`}
+      </h2>
+      <button onClick={reset}>Start Over</button>
       <div className="LightsOut" data-testid="LightsOut">
         {getRows()}
       </div>
     </div>
-  )
+  );
 };
 
 export default LightsOut;
